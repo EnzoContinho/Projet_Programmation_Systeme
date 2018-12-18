d4831115e8a72296aed6bb5712702312668e295d
HartFlash <37220438+HartFlash@users.noreply.github.com>
2018-12-18 08:32:45 +0100

    Update types.h
    Merge de nos types.h

--- types.h
+++ types.h
@@ -16,7 +16,56 @@
 
 #define MAX_LGR_NOM 1024   /* Longueur maximale d'un nom de fichier absolu  */
 #define FICHIER_CLE "fichier_SMP"
+#define FICHIER_CLE_FILE "cle.file"
 #define LETTRE_CODE 'a'
+#define NB_THEME_MIN 2
+#define NB_ARCHIVISTES_MIN 2
+#define NB_THEME_MAX 100
+#define NB_ARCHIVISTES_MAX 100
+
+
+typedef struct
+{
+    long type;       /*Type 1 => consultation ; 
+                       Type 2 => publication*/
+    int journaliste; /*Id du journaliste entre 10 000 & 20 000*/
+    int mode;        /*Seulement pour le type 2 (si mode=1 => ajout)
+                                       sinon mode=2 => suppression)*/
+    int theme;       /*numéro du thème de l'article*/
+    int num_article; /*numéro de l'article; 
+                       Dans le cas d'un consultation 
+                       ou d'une suppresion */
+    char* texte;    /*Dans le cas de l'ajout*/
+} 
+requete_t;
+
+typedef struct
+{
+    long type;
+    int archiviste; /*Id de l'archiviste*/
+    char* resu;
+} 
+reponse_t;
+
+/* Couleurs dans xterm                                     */
+#define couleur(param) printf("\033[%sm",param)
+
+#define NOIR  "30"
+#define ROUGE "31"
+#define VERT  "32"
+#define JAUNE "33"
+#define BLEU  "34"
+#define CYAN  "36"
+#define BLANC "37"
+#define REINIT "0"
+
+
+/* 
+   Utilisation (pour ecrire en rouge) :
+   
+   couleur(ROUGE); fprintf(stdout,"Hello\n"); couleur(REINIT);
+ 
+*/
 
 void mon_sigaction(int signal, void (*f)(int)){
     struct sigaction action;
